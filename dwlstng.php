9070391464f3bcb30d20b0f019437efacedbc2b2
Dave Ross <dave@davidmichaelross.com>
2015-01-24 23:10:12 -0500

    Renamed include files

--- dwlstng.php
+++ dwlstng.php
@@ -16,8 +16,8 @@ define( 'DWLS_TNG_VERSION', '0.1.1' );
 define( 'DWLS_TNG_URL', plugin_dir_url( __FILE__ ) );
 define( 'DWLS_TNG_PATH', dirname( __FILE__ ) );
 
-include DWLS_TNG_PATH . '/inc/class-daveswordpresslivesearchfrontend.php';
+include DWLS_TNG_PATH . '/inc/front-end.php';
 
 if ( is_admin() || ( defined( 'DOING_DWLS_UNIT_TESTS' ) && DOING_DWLS_UNIT_TESTS ) ) {
-	include DWLS_TNG_PATH . '/inc/daveswordpresslivesearchadmin.php';
+	include DWLS_TNG_PATH . '/inc/admin.php';
 }
