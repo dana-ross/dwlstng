d4f9ae0000905e652352c0ff7947d89205fd5671
Dave Ross <dave@davidmichaelross.com>
2015-02-21 00:19:44 -0500

    Filenames for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,6 @@ define( 'ABSPATH', '/var/www/wordpress/' );
 define( 'DWLS_UNIT_TEST', true );
 
 include realpath( __DIR__ . '/../inc/sanitize.php' );
-include realpath( __DIR__ . '/../inc/class-daveswordpresslivesearchfrontend.php' );
-include realpath( __DIR__ . '/../inc/daveswordpresslivesearchadmin.php' );
+include realpath( __DIR__ . '/../inc/front-end.php' );
+include realpath( __DIR__ . '/../inc/admin.php' );
 require realpath( __DIR__ . '/../vendor/autoload.php' );
