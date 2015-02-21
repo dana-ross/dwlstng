6ac30be6162c424cd3a0436e8d9ef28d18c424db
Dave Ross <dave@davidmichaelross.com>
2015-02-21 00:14:18 -0500

    paths

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 
-define('ABSPATH', '/var/www/wordpress/');
-define('DWLS_UNIT_TEST', true);
+define( 'ABSPATH', '/var/www/wordpress/' );
+define( 'DWLS_UNIT_TEST', true );
 
-include __DIR__ . '/../inc/sanitize.php';
-include __DIR__ . '/../inc/class-daveswordpresslivesearchfrontend.php';
-include __DIR__ . '/../inc/daveswordpresslivesearchadmin.php';
-require __DIR__ . '/../vendor/autoload.php';
+include realpath( __DIR__ . '/../inc/sanitize.php' );
+include realpath( __DIR__ . '/../inc/class-daveswordpresslivesearchfrontend.php' );
+include realpath( __DIR__ . '/../inc/daveswordpresslivesearchadmin.php' );
+require realpath( __DIR__ . '/../vendor/autoload.php' );
