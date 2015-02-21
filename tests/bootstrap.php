fc8daf426efa82afd43de525a7dd411df3fceff6
Dave Ross <dave@davidmichaelross.com>
2015-02-21 00:10:41 -0500

    Fix paths in bootstrap.php that broke unit testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 define('ABSPATH', '/var/www/wordpress/');
 define('DWLS_UNIT_TEST', true);
 
-include __DIR__ . '../inc/sanitize.php';
-include __DIR__ . '../inc/class-daveswordpresslivesearchfrontend.php';
-include __DIR__ . '../inc/daveswordpresslivesearchadmin.php';
-require __DIR__ . '../vendor/autoload.php';
+include __DIR__ . '/../inc/sanitize.php';
+include __DIR__ . '/../inc/class-daveswordpresslivesearchfrontend.php';
+include __DIR__ . '/../inc/daveswordpresslivesearchadmin.php';
+require __DIR__ . '/../vendor/autoload.php';
